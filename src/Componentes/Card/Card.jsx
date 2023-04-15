30e999bd0c21d3188a2a0fc6488cfbccd3eeaa98
JhohanCortes <dicenadie2@gmail.com>
2023-04-15 13:07:48 -0500

    onClose Fixed

--- src/Componentes/Card/Card.jsx
+++ src/Componentes/Card/Card.jsx
@@ -6,13 +6,13 @@ const Card = ({ name, image, id, onClose }) => {
   return (
     <div className="card">
       
-      <Link to={`/detail/${id}`}>
         <div className="card-image-wrapper">
           <button  className="card-button" onClick={() => onClose(id)}>X</button>
+      <Link to={`/detail/${id}`}>
           <div className="card-name">{name}</div>
           <img src={image} alt={name} />
-        </div>
       </Link>
+        </div>
     </div>
   );
 };
